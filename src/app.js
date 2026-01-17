fa124ab013bf8d5c2ffd9beeffa78449e15f759b
Mahmoud <mahmoudnasser1561@gmail.com>
2026-01-17 14:54:34 +0200

    basic calc code

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function calc(a, b, op) {
+  switch (op) {
+    case '+': return a + b;
+    case '-': return a - b;
+    case '*': return a * b;
+    case '/': return a / b;
+    default: throw new Error('Invalid operator');
+  }
+}
+
+module.exports = calc;
